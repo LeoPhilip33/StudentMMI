03eef203c6d14b03cc4c7fb07f959226bacddbf7
Amandedu33_ <leo.philip33.bordeaux@gmail.com>
2020-02-22 18:04:15 +0100

    c'est quoi le probleme

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <title>Inscription utilisateur </title>
+    <title>Inscription/Connection</title>
     <meta charset='utf-8'>
     <link href="style.css" rel="stylesheet">
     </head>
     <body>
-        <h1>Bienvenue sur mon site internet</h1>
-        <form name="inscription" method="post" action="send.php">
-            <div class="bloc">
-                <span id="a">  gegerger zgegue :</span><input type="text" name="nom" required /><br>
-                <span id="b">Enter votre Prénom :</span><input type="text" name="prenom" required /><br>
-                <span id="c">Enter votre Email :</span><input type="text" name="email" required  /><br>
-                <span id="d">Enter votre Mot de passe :</span><input type="password" name="mdp" required /><br>
-                <input type="submit" name="valider" value="S'inscrire" id="valider" />
-            </div>
-        </form>
+        <div class="box">
+          <div>One</div>
+          <div>Two</div>
+          <div>Three
+              <br>has
+              <br>extra
+              <br>text
+          </div>
+        </div>
     </body>
 </html>
