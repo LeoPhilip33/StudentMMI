692f093c16c616861318f9aa5569a3d2abb1f4ac
Amandedu33_ <leo.philip33.bordeaux@gmail.com>
2020-02-22 21:51:42 +0100

    new_site

--- index.php
+++ index.php
@@ -6,15 +6,24 @@
         <link href="style.css" rel="stylesheet">
     </head>
     <body> 
-        <div class="page1">
-            <a href="#page2"><div class="connection"> Se connecter </a></div>
-            <a href="#page3"><div class="inscription"> S'inscrire </a></div>
-        </div>
-        <div id="page2">
-            <h1> Vous etes dans la page 2 </h1>
-        </div>
-        <div id="page3">
-            <h1> Vous etes dans la page 3 </h1>
+        <div class="register">
+            <div class="gauche">
+                <h1> text gauche </h1>
+            </div>
+            <div class="droit">
+                <h2> Inscrivez vous gratuitement et devenez membre </h2>
+                <h3> Créer vous un compte. C'est gratuit ! </h3>
+                <div class="formulaire">
+                    <div class="alignement">
+                        <div class="nom"><input type="text" name="Nom" maxlength="30"  placeholder="Nom" required> </div>
+                        <div><input type="text" name="Prenom" maxlength="25" placeholder="Prénom" required> </div>
+                    </div>
+                    <div><input type="text" name="Email" minlength="4" maxlength="25" placeholder="Email" required><div>
+                    <input type="text" name="Mdp" minlength="4" maxlength="25" placeholder="Mot de passe" required><div>
+                    <input type="text" name="confMdp" minlength="4" maxlength="25" placeholder="Confirmation" required><div>
+                    <input id="button" type="submit" value="Enregistrez vous maintenant !">
+                </div>
+            </div>
         </div>
     </body>
 </html>
