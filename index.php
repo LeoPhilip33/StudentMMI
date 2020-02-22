be16fbe9a71a8ada83f9135313084fd36a1e748e
Amandedu33_ <leo.philip33.bordeaux@gmail.com>
2020-02-22 20:46:55 +0100

    start

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <title>Inscription/Connection</title>
-    <meta charset='utf-8'>
-    <link href="style.css" rel="stylesheet">
+        <title>Inscription/Connection</title>
+        <meta charset='utf-8'>
+        <link href="style.css" rel="stylesheet">
     </head>
-    <body>
-        <div class="box">
-            <div class="connection"><a href="" id = "bleu" > Se connecter </a></div>
-            <div class="inscription"><a href="#" id = "blanc"> S'inscrire </a></div>
+    <body> 
+        <div class="page1">
+            <a href="#page2"><div class="connection"> Se connecter </a></div>
+            <a href="#page3"><div class="inscription"> S'inscrire </a></div>
+        </div>
+        <div id="page2">
+            <h1> Vous etes dans la page 2 </h1>
+        </div>
+        <div id="page3">
+            <h1> Vous etes dans la page 3 </h1>
         </div>
     </body>
 </html>
